d72b46a63963a09a48bd80c3316fa65b53af6ee2
Millan Ouma <millanouma@gmail.com>
2023-06-15 12:38:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 
-int main(void) {
-   char str1[55];
-    strcpy (str1, "\"Programming is like building a multilingual puzzle\n");
-    puts (str1);
-   
-
-   return(0);
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
