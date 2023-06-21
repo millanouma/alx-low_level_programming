4d9f85e5903cefd2268aff848175d663317f2fcc
Millan Ouma <millanouma@gmail.com>
2023-06-20 19:49:27 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,11 @@
  * Description - use only _putchar
  *
  * Return: Always 0 (Success)
- */
-int main(void)
+*/
+void print_alphabet(void)
 {	char ch;
 	for(ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
 	_putchar('\n');
-	return (0);
+	
 }
