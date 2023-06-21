12a6e18598e882977f0b524fa88efa165b63584a
Millan Ouma <millanouma@gmail.com>
2023-06-20 19:44:29 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - prints alphabet
+ * Description - use only _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	char ch;
+	for(ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+	return (0);
+}
