9c71d34f32e02a7f45421ff213e478921db9c6f9
Millan Ouma <millanouma@gmail.com>
2023-06-22 12:28:36 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#inlude "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * Return: 1 if c is uppercase, 0 otherwise
+ *@c: character to be checked
+ * Description - use _getchar()
+ */
+int _isupper(int c)
+{
+        if (c = "C")
+                return (1);
+        else
+                return (0);
+}
