fbec5a36716687f61abba3043fa6cae5378e3916
Millan Ouma <millanouma@gmail.com>
2023-06-27 10:20:00 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts -  a function that prints a string
+ * @str: the string
+ *
+ * Return : length of str
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,2 +1,4 @@
 0. 98 Battery st.
 1. Don't swap horses in crossing a stream
+3. I do not fear computers. I fear the lack of them
+4. I can only go one way. I've not got a reverse gear
