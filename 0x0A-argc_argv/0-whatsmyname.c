9073c413f137a995a1513e0bfa58dfd4f894ca8e
Millan Ouma <millanouma@gmail.com>
2023-07-11 13:52:07 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,10 @@
  *
  * Return : 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv[])
 {
-	printf("%s\n",argv[0]);
+	(void)argc;
+
+	printf("%s\n", argv[0]);
 	return (0);
 }
