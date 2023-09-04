84b8f1085fbc42b64ab0cd02f9b2c6731affbdc2
millanouma <millanouma@gmail.com>
2023-09-04 07:08:13 -0700

    third task

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,7 +4,7 @@
 /**
  * main - function  prints the number of arguments passed into it.
  * @argc: number of arguments
- * @argv[]: array of arguments
+ * @argv: array of arguments
  *
  * Return: Always 0 (success)
  */
