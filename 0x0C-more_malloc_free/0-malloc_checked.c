b676da113833d4229d555f6e2b4278f1b9aebb5a
millanouma <millanouma@gmail.com>
2023-09-07 04:41:12 -0700

    first task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *malloc_checked - allocate memory
+ * @b: size of bytes to allocate
+ *
+ * Return: pointer to allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
