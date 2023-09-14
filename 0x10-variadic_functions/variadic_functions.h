492ac8d8187e7cc2449659271ab421d488cbf5ae
millanouma <millanouma@gmail.com>
2023-09-14 05:41:13 -0700

    second task

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,7 +2,7 @@
 #define VAR_FUNC_H
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
-
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #include <stdio.h>
 #include <stdarg.h>
