37fb69a60b04160e6ba3d09eb16cd567711504d0
millanouma <millanouma@gmail.com>
2023-09-14 05:18:00 -0700

    first task

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all -  function that returns the
+ *  sum of all its parameters.
+ * @ unsigned int n - number of parameters
+ *
+ * Return: Sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int n;
+	int sum = 0;
+	va_list list;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
+	va_end(list);
+
+	return (sum);
+}
+
