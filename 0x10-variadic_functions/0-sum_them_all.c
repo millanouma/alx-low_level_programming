4d0c267f3f77593eb10ab134ad8c3ce98f704a55
millanouma <millanouma@gmail.com>
2023-09-14 05:24:51 -0700

    description for n

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,6 @@
 #include "variadic_functions.h"
 /**
- * sum_them_all -  function that returns the
- *  sum of all its parameters.
+ * sum_them_all - returns sum of all its parameters.
  * @n - number of parameters
  *
  * Return: Sum
