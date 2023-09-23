899282a089d7fad21d14701627b3730f00e2ca9d
millanouma <millanouma@gmail.com>
2023-09-22 18:16:52 -0700

    task 2

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -9,6 +8,7 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
     char *str;
@@ -18,6 +18,6 @@ typedef struct list_s
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
-
+size_t list_len(const list_t *h);
 
 #endif
